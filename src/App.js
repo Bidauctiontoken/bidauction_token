50b9cf680ad2d0547881b29bef346edc140e7b18
sundayonah <sundayonah94@gmail.com>
2023-02-16 22:37:18 +0100

    site blank on production

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import Header from "./COMPONENTS/Header";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Project from "./COMPONENTS/Pages/Project";
 
-import { Web3ReactProvider } from "@web3-react/core";
-import { Web3Provider } from "@ethersproject/providers";
-
 export default function Home(provider) {
-  
   return (
     <>
       <Router>
