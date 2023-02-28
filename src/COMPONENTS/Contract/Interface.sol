f6a79a310c233ce103dd845c005e7b7d9296f9b5
sundayonah <sundayonah94@gmail.com>
2023-02-28 16:44:16 +0100

    change abi.json

--- src/COMPONENTS/Contract/Interface.sol
+++ src/COMPONENTS/Contract/Interface.sol
@@ -1,5 +1,5 @@
 /**
- *Submitted for verification at BscScan.com on 2023-02-27
+ *Submitted for verification at BscScan.com on 2023-02-28
 */
 
 // SPDX-License-Identifier: MIT
@@ -60,6 +60,7 @@ contract Ownable is Context {
         address msgSender = _msgSender();
         _owner = msgSender;
     }
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     function owner() public view returns (address) {
         return _owner;
@@ -68,6 +69,36 @@ contract Ownable is Context {
     modifier onlyOwner() {
         require(_owner == _msgSender(), "Ownable: caller is not the owner");
         _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
     }
 
 }
